a45c8e8354755a83a8d8094c70cdcfbbd7d587e4
AlankritaShah <alankrita18.as@gmail.com>
2020-04-19 20:13:17 +0530

    memoization used for Robot in a Grid

--- DynamicProgramming/RobotInAGrid/RobotInAGrid.java
+++ DynamicProgramming/RobotInAGrid/RobotInAGrid.java
@@ -1,26 +1,31 @@
 package DynamicProgramming.RobotInAGrid;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RobotInAGrid {
 	private ArrayList<Point> getPath(boolean[][] grid)
 	{
 		ArrayList<Point> path = new ArrayList<>();
-		if(getPath(grid, grid.length-1, grid[0].length-1, path))
+		HashMap<Point, Boolean> failedPath = new HashMap<>();
+		if(getPath(grid, grid.length-1, grid[0].length-1, path, failedPath))
 			return path;	
 		return null;
 	}
 	
-	
-	private boolean getPath(boolean[][] grid, int r, int c, ArrayList<Point> path) {
+	private boolean getPath(boolean[][] grid, int r, int c, ArrayList<Point> path, HashMap<Point, Boolean> failedPath) {
 		if(r < 0 || c < 0 || !grid[r][c])
 			return false;
 		
 		boolean isOrigin = r==0 && c==0;
+		Point point = new Point(r, c);
+		if(failedPath.containsKey(point))
+			return false;
 		
-		if(isOrigin || getPath(grid, r-1, c, path) || getPath(grid, r, c-1, path)) {
-			path.add(new Point(r, c));
+		if(isOrigin || getPath(grid, r-1, c, path, failedPath) || getPath(grid, r, c-1, path, failedPath)) {
+			path.add(point);
 			return true;
 		}
+		failedPath.put(point, false);
 		return false;
 	}
 	
@@ -28,14 +33,12 @@ public class RobotInAGrid {
 	{
 		long startTime = System.currentTimeMillis();
 		
-		boolean[][] grid = new boolean[5][5];
-		for(int i=0;i<5;i++) {
-			for(int j=0;j<5;j++) {
+		boolean[][] grid = new boolean[10][10];
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<10;j++) {
 				grid[i][j] = true;
 			}
 		}
-		
-		grid[0][2] = false;
 		
 		ArrayList<Point> path = new RobotInAGrid().getPath(grid);
 		if(path!=null)
